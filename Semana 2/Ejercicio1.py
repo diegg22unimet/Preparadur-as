2d85d685985df8fb2867b19a7c5ee4a875b0fbed
diegg22unimet <dmanzanilla@correo.unimet.edu.ve>
2022-09-22 11:19:11 -0400

    Second commit

---
+++ Semana 2/Ejercicio1.py
@@ -0,0 +1,8 @@
+temperatura = float(input("Ingrese una temperatura en grados Celsius: "))
+
+if temperatura > 10:
+    print("Hace calor")
+elif temperatura == 10:
+    print("El clima está fresco")
+else:
+    print("Hace frío")
