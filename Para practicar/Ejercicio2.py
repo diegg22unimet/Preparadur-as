9a231df20385768704a5877d031abbfc52913fb3
diegg22unimet <dmanzanilla@correo.unimet.edu.ve>
2022-09-26 09:28:56 -0400

    First commit

---
+++ Para practicar/Ejercicio2.py
@@ -0,0 +1,6 @@
+#Escribir un programa que pregunte al usuario su edad y muestre por pantalla todos los años que ha cumplido (desde 1 hasta su edad).
+edad = int(input("Ingrese su edad: "))
+i = 1
+while i <= edad:
+    print(f"Usted cumplió {i} años")
+    i += 1
